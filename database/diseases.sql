93b1e8e1152ecbe75daccb1a936faab097e157e6
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2023-06-03 10:13:09 +0100

    Add files via upload

---
+++ database/diseases.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.diseases (name, description) VALUES
+('Powdery Mildew', 'A fungal disease that affects the leaves, stems, and flowers of many plants. It appears as a white or gray powdery coating on the plant surface.'),
+('Bacterial Wilt', 'A bacterial disease that causes the wilting and death of infected plants. It is spread by insects and contaminated soil.'),
+('Fusarium Wilt', 'A fungal disease that causes wilting, yellowing, and stunting of infected plants. It is spread by contaminated soil, seeds, and plant debris.'),
+('Leaf Spot', 'A fungal disease that causes circular or irregular spots on the leaves of infected plants. It can lead to defoliation and reduced yield.'),
+('Aphids', 'Small, soft-bodied insects that feed on the sap of plants. They can cause distortion, stunting, and yellowing of leaves, as well as the transmission of viral diseases.'),
+('Spider Mites', 'Tiny, spider-like insects that feed on the sap of plants. They can cause yellowing, stippling, and webbing on leaves, as well as the transmission of viral diseases.'),
+('Whitefly', 'Small, whiteflies that feed on the sap of plants. They can cause yellowing, stunting, and sooty mold on leaves, as well as the transmission of viral diseases.'),
+('Black Spot', 'A fungal disease that causes black spots on the leaves of infected plants. It can lead to defoliation and reduced yield.'),
+('Rust', 'A fungal disease that causes rusty-colored spots on the leaves and stems of infected plants. It can lead to defoliation and reduced yield.');
+
+select * from crops;
